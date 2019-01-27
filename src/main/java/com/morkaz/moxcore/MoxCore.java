6a67e03b6c15da0f826c29a826776ba2860f1169
BuildTools <unconfigured@null.spigotmc.org>
2019-01-27 15:41:35 +0100

    Metrics update

--- src/main/java/com/morkaz/moxcore/MoxCore.java
+++ src/main/java/com/morkaz/moxcore/MoxCore.java
@@ -1,15 +1,25 @@
 package com.morkaz.moxcore;
 
+import com.morkaz.moxcore.misc.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MoxCore extends JavaPlugin {
 
+	private Metrics metrics;
+
+	@Override
 	public void onEnable(){
+		//Add Metrics
+		metrics = new Metrics(this);
+
+		//Ending
 		Bukkit.getLogger().info("["+getDescription().getName()+"] Plugin enabled!");
 	}
 
+	@Override
 	public void onDisable(){
+		//Ending
 		Bukkit.getLogger().info("["+getDescription().getName()+"] Plugin disabled!");
 	}
 
